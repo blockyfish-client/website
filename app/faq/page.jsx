dbc61215520a0b548ae4ccf6a5755a54b4b662e7
Pi <piisthebestthingintheuniverse@gmail.com>
2023-12-23 14:04:36 -0800

    create faq page

---
+++ app/faq/page.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from "../components/navbar";
+
+export default function Faq() {
+	return (
+		<main className="bg-gray-900 text-white min-h-screen w-[calc(100vw-1rem)] flex flex-col items-center select-none pb-8 px-4">
+			<Navbar />
+		</main>
+	);
+}
