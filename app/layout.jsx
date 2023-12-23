348ce034b76daaac007ceaccf91f8bff101301e0
Pi <piisthebestthingintheuniverse@gmail.com>
2023-12-23 14:04:14 -0800

    scrollbar always visible

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
 				className={quicksand.className}
 				style={{
 					overflowX: "hidden",
-					overflowY: "auto"
+					overflowY: "scroll"
 				}}
 			>
 				{children}
