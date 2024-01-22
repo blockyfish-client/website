12e9d2d99d6575c41b4988617432c6e53faf85d6
Pi <piisthebestthingintheuniverse@gmail.com>
2024-01-21 17:39:14 -0800

    try returning request's actual location

--- pages/api/apispoof/servers/l.js
+++ pages/api/apispoof/servers/l.js
@@ -1,3 +1,5 @@
+import ipLocation from "iplocation";
+const requestIp = require("request-ip");
 import NextCors from "nextjs-cors";
 
 export default async function handler(req, res) {
@@ -6,5 +8,10 @@ export default async function handler(req, res) {
 		origin: "*",
 		optionsSuccessStatus: 200
 	});
-	res.status(200).json([37.751, -97.822, "US"]);
+	try {
+		const ipData = await ipLocation(requestIp.getClientIp(req));
+		res.status(200).json([ipData.latitude, ipData.longitude, ipData.country.code]);
+	} catch {
+		res.status(200).json([37.751, -97.822, "US"]);
+	}
 }
