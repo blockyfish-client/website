4074c7807cc420cba30374a5f5ea5cc03a155809
Pi <piisthebestthingintheuniverse@gmail.com>
2024-02-16 19:36:42 -0800

    fixed embed

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,14 +6,63 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const quicksand = Quicksand({ subsets: ["latin"] });
 
-export const metadata = {
-	title: "Blockyfish Client",
-	description: "Deeeep.io's best client mod"
-};
-
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<head>
+				<title>Blockyfish Client</title>
+				<meta
+					name="description"
+					content="Deeeep.io's best client mod"
+				/>
+
+				<meta
+					property="og:url"
+					content="https://blockyfish.vercel.app/"
+				/>
+				<meta
+					property="og:type"
+					content="website"
+				/>
+				<meta
+					property="og:title"
+					content="Blockyfish Client"
+				/>
+				<meta
+					property="og:description"
+					content="Deeeep.io's best client mod"
+				/>
+
+				<meta
+					property="og:image"
+					content="https://blockyfish.vercel.app/icon.png"
+				/>
+
+				<meta
+					name="twitter:card"
+					content="summary_large_image"
+				/>
+				<meta
+					property="twitter:domain"
+					content="blockyfish.vercel.app"
+				/>
+				<meta
+					property="twitter:url"
+					content="https://blockyfish.vercel.app/"
+				/>
+				<meta
+					name="twitter:title"
+					content="Blockyfish Client"
+				/>
+				<meta
+					name="twitter:description"
+					content="Deeeep.io's best client mod"
+				/>
+				<meta
+					name="twitter:image"
+					content="https://blockyfish.vercel.app/icon.png"
+				/>
+			</head>
 			<body
 				className={quicksand.className}
 				style={{
