7f05d84f1a452ca3dc82586ce09b336941dfe138
Pi <piisthebestthingintheuniverse@gmail.com>
2024-05-03 09:55:46 -0700

    update hosts id

--- pages/api/apispoof/regions.js
+++ pages/api/apispoof/regions.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 	await NextCors(req, res, {
 		methods: ["GET"],
 		origin: "*",
-		optionsSuccessStatus: 200
+		optionsSuccessStatus: 200,
 	});
 	res.status(200).json(
 		// [
@@ -15,11 +15,36 @@ export default async function handler(req, res) {
 		// 	{ id: "OCE", latitude: -33.9256, longitude: 151.191, gamemodes: ["1"] }
 		// ]
 		[
-			{ id: "AS", latitude: 35.5826, longitude: 139.746, gamemodes: ["6"] },
-			{ id: "EU", latitude: 52.35, longitude: 4.9167, gamemodes: ["1"] },
-			{ id: "LAS", latitude: -23.5338, longitude: -46.6253, gamemodes: ["1"] },
-			{ id: "NAE", latitude: 40, longitude: -74, gamemodes: ["1", "6"] },
-			{ id: "OCE", latitude: -33.9256, longitude: 151.191, gamemodes: ["1"] }
+			// {
+			// 	id: "AS",
+			// 	latitude: 35.5826,
+			// 	longitude: 139.746,
+			// 	gamemodes: ["6"],
+			// },
+			{
+				id: "EU",
+				latitude: 52.35,
+				longitude: 4.9167,
+				gamemodes: ["1"],
+			},
+			{
+				id: "LAS",
+				latitude: -23.5338,
+				longitude: -46.6253,
+				gamemodes: ["1"],
+			},
+			{
+				id: "NAE",
+				latitude: 40,
+				longitude: -74,
+				gamemodes: ["1", "6"],
+			},
+			{
+				id: "OCE",
+				latitude: -33.9256,
+				longitude: 151.191,
+				gamemodes: ["1"],
+			},
 		]
 	);
 }
