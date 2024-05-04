5717172d98e4ef4db4fa838006b64ace8386508d
Pi <piisthebestthingintheuniverse@gmail.com>
2024-05-04 12:11:34 -0700

    update timestamps

--- pages/api/apispoof/auth/timezone.js
+++ pages/api/apispoof/auth/timezone.js
@@ -4,7 +4,9 @@ export default async function handler(req, res) {
 	await NextCors(req, res, {
 		methods: ["GET"],
 		origin: "*",
-		optionsSuccessStatus: 200
+		optionsSuccessStatus: 200,
 	});
-	res.status(200).json({ t: "52765d755740523629524a297d453232296e3668635143506d405b487049734f705b546f" });
+	res.status(200).json({
+		t: "6e316a4357697248293c7e50604e464170675e3d37463742486e453c336b327175574655",
+	});
 }
