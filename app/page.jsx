6d221cd53aceb7cb19e027acebc37e75ed8e8c4f
Pi <piisthebestthingintheuniverse@gmail.com>
2024-05-12 23:41:24 -0700

    add android download

--- app/page.jsx
+++ app/page.jsx
@@ -60,7 +60,7 @@ export default function Home() {
 						Download
 					</p>
 					<Link
-						className="flex justify-center items-center gap-3 bg-blue-500 hover:bg-blue-600 shadow-lg px-4 py-3 border-b-4 border-blue-600 hover:border-blue-700 rounded-2xl text-white transition-colors"
+						className="flex justify-center items-center gap-3 bg-blue-600 hover:bg-blue-700 shadow-lg px-4 py-3 border-b-4 border-blue-700 hover:border-blue-800 rounded-2xl text-white transition-colors"
 						href={
 							"https://github.com/blockyfish-client/desktop-client/releases/download/v3.2.1/blockyfish-client-win-3.2.1.exe"
 						}
@@ -78,7 +78,7 @@ export default function Home() {
 						Download for Windows (x64)
 					</Link>
 					<Link
-						className="flex justify-center items-center gap-3 bg-blue-500 hover:bg-blue-600 shadow-lg px-4 py-3 border-b-4 border-blue-600 hover:border-blue-700 rounded-2xl text-white transition-colors"
+						className="flex justify-center items-center gap-3 border-gray-700 hover:border-gray-800 bg-gray-600 hover:bg-gray-700 shadow-lg px-4 py-3 border-b-4 rounded-2xl text-white transition-colors"
 						href={
 							"https://github.com/blockyfish-client/desktop-client/releases/download/v3.2.1/blockyfish-client-mac-3.2.1.dmg"
 						}
@@ -97,7 +97,7 @@ export default function Home() {
 						Download for MacOS (Universal)
 					</Link>
 					<Link
-						className="flex justify-center items-center gap-3 bg-blue-500 hover:bg-blue-600 shadow-lg px-4 py-3 border-b-4 border-blue-600 hover:border-blue-700 rounded-2xl text-white transition-colors"
+						className="flex justify-center items-center gap-3 border-orange-700 hover:border-orange-800 bg-orange-600 hover:bg-orange-700 shadow-lg px-4 py-3 border-b-4 rounded-2xl text-white transition-colors"
 						href={
 							"https://github.com/blockyfish-client/desktop-client/releases/download/v3.2.1/blockyfish-client-linux-3.2.1.tar.xz"
 						}
@@ -114,7 +114,24 @@ export default function Home() {
 						</svg>
 						Download for Linux (x64)
 					</Link>
-					<p>Android support is coming soon!</p>
+					<Link
+						className="flex justify-center items-center gap-3 border-emerald-700 hover:border-emerald-800 bg-emerald-600 hover:bg-emerald-700 shadow-lg mt-4 px-4 py-3 border-b-4 rounded-2xl text-white transition-colors"
+						href={
+							"https://github.com/blockyfish-client/android-client/releases/download/v1.0.0/app-release.apk"
+						}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="currentColor"
+							viewBox="0 0 24 24"
+						>
+							<title>🤖</title>
+							<path d="M16.61 15.15C16.15 15.15 15.77 14.78 15.77 14.32S16.15 13.5 16.61 13.5H16.61C17.07 13.5 17.45 13.86 17.45 14.32C17.45 14.78 17.07 15.15 16.61 15.15M7.41 15.15C6.95 15.15 6.57 14.78 6.57 14.32C6.57 13.86 6.95 13.5 7.41 13.5H7.41C7.87 13.5 8.24 13.86 8.24 14.32C8.24 14.78 7.87 15.15 7.41 15.15M16.91 10.14L18.58 7.26C18.67 7.09 18.61 6.88 18.45 6.79C18.28 6.69 18.07 6.75 18 6.92L16.29 9.83C14.95 9.22 13.5 8.9 12 8.91C10.47 8.91 9 9.24 7.73 9.82L6.04 6.91C5.95 6.74 5.74 6.68 5.57 6.78C5.4 6.87 5.35 7.08 5.44 7.25L7.1 10.13C4.25 11.69 2.29 14.58 2 18H22C21.72 14.59 19.77 11.7 16.91 10.14H16.91Z" />
+						</svg>
+						Download for Android
+					</Link>
 					<p>
 						Join the{" "}
 						<Link
