ace79697065c76ee0319cbf7953f1881eebf17fb
Pi <piisthebestthingintheuniverse@gmail.com>
2024-06-02 18:24:50 -0700

    analytics: remove stuff

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,6 @@
 import { Quicksand } from "next/font/google";
 import "./globals.css";
 
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { YoutubeViewbot } from "./components/youtubeViewbot";
 
 const quicksand = Quicksand({ subsets: ["latin"] });
@@ -59,8 +57,6 @@ export default function RootLayout({ children }) {
 				}}
 			>
 				{children}
-				<Analytics />
-				<SpeedInsights />
 				<YoutubeViewbot />
 			</body>
 		</html>
